af3c4d1f92bbb6a7f9d53d07e7d77ea7db7ada52
antjamespadgett <padgettanthonyj@gmail.com>
2021-10-20 13:34:16 -0500

    added a few steps

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hamsterName = "Rubble"; 
+            int hamsterAge = 8;
+            char hamsterNickname = 'R';
+            bool spinningWheel = true;
+            decimal hamstersWeight = 5.2m;
+
+            Console.WriteLine($"My hamsters name is {hamsterName}, she is {hamsterAge} years old and has a nickname {hamsterNickname}. Its {spinningWheel} she loves to spin in her wheel and weighs {hamstersWeight} pounds.. a chubby hamster!! ");
+
+
+            Console.ReadLine();
+
+
+
+
+
+            
+
+
+        }
+    }
+}
